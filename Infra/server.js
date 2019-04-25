36e481f9acfd3b7ba80eb24542cff30f0cca29be
alinemorelli <aline.morelliw@gmail.com>
2019-04-25 02:32:30 -0300

    feat: add cors to express

--- Infra/server.js
+++ Infra/server.js
@@ -3,11 +3,13 @@ require('dotenv').config()
 const express = require('express')
 const apiConfig = require('./config/api')
 const initDatabase = require('./api/init/database')
+const cors = require('cors')
 
 initDatabase()
   .then(_ => {
     const app = express()
     app.use(express.json())
+    app.use(cors())
 
     app.use(require('./api/routes'))
 
